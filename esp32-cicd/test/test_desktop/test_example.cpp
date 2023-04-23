f748368212d9d11c2bac9f1f69506bf7643838b3
isfandyar01 <isfandyar0@gmail.com>
2023-04-23 11:46:51 +0500

    declared rms_to_peak

--- esp32-cicd/test/test_desktop/test_example.cpp
+++ esp32-cicd/test/test_desktop/test_example.cpp
@@ -8,6 +8,10 @@ void test_add_two_ints( void)
     int sum = add(1,4);
     TEST_ASSERT_EQUAL_INT32(5,sum);
 }
+void rms_to_peak_current(void)
+{
+    
+}
 
 void RUN_UNITY_TESTS() {
     UNITY_BEGIN();
