3569d1926514b1724e2013a46bdb3f446f42d10c
isfandyar01 <isfandyar0@gmail.com>
2023-04-24 13:46:16 +0500

    added implementation of secondary peak current

--- esp32-cicd/src/YHDC.hpp
+++ esp32-cicd/src/YHDC.hpp
@@ -2,7 +2,7 @@
 #include <stdint.h>
 
 static float primary_peak_current ;
-
+static float secondary_peak_current;
 /**
  * @brief for testing that unity test framework is working
  *
@@ -20,7 +20,7 @@ int add (int a, int b)
  * @brief returns converted RMS current to peak
  * current by multiplying it 1.414
  * @param current is current range of sensors 0 - 100A
- * @return float 
+ * @return float primcary coil peak current
  */
 
 float pri_peak_current(uint8_t current=100)
@@ -34,6 +34,6 @@ float pri_peak_current(uint8_t current=100)
  * @return float 
  */
 float sec_peak_current()
-{
-    return 1.0f;
+{   secondary_peak_current= primary_peak_current/2000; // 2000 is number of turns of sensor
+    return secondary_peak_current;
 }
