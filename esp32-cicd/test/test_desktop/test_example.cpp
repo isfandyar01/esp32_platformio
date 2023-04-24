ed5c6c92c841254c7f44e92c3eb3ff6f65a5f661
isfandyar01 <isfandyar0@gmail.com>
2023-04-24 12:45:26 +0500

    added secondary peak current to  fail

--- esp32-cicd/test/test_desktop/test_example.cpp
+++ esp32-cicd/test/test_desktop/test_example.cpp
@@ -8,16 +8,22 @@ void test_add_two_ints( void)
     int sum = add(1,4);
     TEST_ASSERT_EQUAL_INT32(5,sum);
 }
-void rms_peak_current(void)
-{   float expected = rms_to_peak_current();
+void peak_current(void)
+{   float expected = pri_peak_current();
     TEST_ASSERT_EQUAL_FLOAT(141.4f,expected);
    
+}
+
+void secondary_peak_current()
+{
+    float expected = sec_peak_current();
 }
 
 void RUN_UNITY_TESTS() {
     UNITY_BEGIN();
     RUN_TEST(test_add_two_ints);
-    RUN_TEST(rms_peak_current);
+    RUN_TEST(peak_current);
+    RUN_TEST(secondary_peak_current);
     UNITY_END();
 }
 
