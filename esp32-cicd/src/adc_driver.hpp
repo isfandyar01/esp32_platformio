80d06abcad8c74be6a65b87e608fc24119e23c98
isfandyar01 <isfandyar0@gmail.com>
2023-04-25 12:36:10 +0500

    added adc_driver hpp

--- esp32-cicd/src/adc_driver.hpp
+++ esp32-cicd/src/adc_driver.hpp
@@ -4,12 +4,16 @@ extern "C"
 #include "freertos/FreeRTOS.h"
 #include "esp_adc/adc_continuous.h"
 
-#define ADC_CONT_MODE 
+#define ADC_CONT_MODE 0
 #define ADC_SINGLE_SHOT_MODE
 
 #ifdef ADC_CONT_MODE
 {
-adc_con
+adc_continuous_handle_cfg_t adc_config = {
+    .max_store_buf_size = 1024,
+    .conv_frame_size = 100,
+};
+ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config));
 }
 #endif
 
--- esp32-cicd/src/main.cpp
+++ esp32-cicd/src/main.cpp
@@ -2,7 +2,7 @@ extern "C"
 {
 #include "driver/adc.h"
 
-adc1_config_width(ADC_WIDTH_BIT_12);
+
 
 
 void app_main()
