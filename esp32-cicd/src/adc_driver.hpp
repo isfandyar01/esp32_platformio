d0dab49cdae8352bc7b27cb5dd9d43a7d55323cb
isfandyar01 <isfandyar0@gmail.com>
2023-04-25 12:19:39 +0500

    added adc_driver hpp

---
+++ esp32-cicd/src/adc_driver.hpp
@@ -0,0 +1,19 @@
+extern "C" 
+{
+#include "driver/adc.h"
+#include "freertos/FreeRTOS.h"
+#include "esp_adc/adc_continuous.h"
+
+#define ADC_CONT_MODE 
+#define ADC_SINGLE_SHOT_MODE
+
+#ifdef ADC_CONT_MODE
+{
+adc_con
+}
+#endif
+
+
+
+
+}
--- esp32-cicd/src/main.cpp
+++ esp32-cicd/src/main.cpp
@@ -1,6 +1,8 @@
-extern "C" {
+extern "C" 
+{
+#include "driver/adc.h"
 
-
+adc1_config_width(ADC_WIDTH_BIT_12);
 
 
 void app_main()
